33c41a266edf554bea40cb663a6e5ccfccdfe534
juste-vitu <victor.eugenio@escola.pr.gov.br>
2025-05-20 11:44:04 -0300

    exemplo 2 valor e tipo

---
+++ capitulo 1/exemplo2valoresetipos.py
@@ -0,0 +1,16 @@
+#Tipo inteiro 1,2,3 ... -2.-3 ...
+inteiro = 2
+print(type(inteiro))
+
+#Tipo real ou ponto flutuante float 1.3, 0.6 ... -3.456 ...
+float = 1.3
+print(type(float))
+
+#Caracteres
+char1 = 'a'
+char2 = '2'
+print(type(char1))
+
+#String conjunto de caracteres
+stringNome = "Vitin Juste"
+print(type(stringNome))
