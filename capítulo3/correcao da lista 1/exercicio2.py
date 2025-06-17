cce0b4dff09e72e3711c51150fe044caff47b010
juste-vitu <victor.eugenio@escola.pr.gov.br>
2025-06-17 11:53:18 -0300

    exercicio 2 da revisão

--- capítulo3/correcao da lista 1/exercicio2.py
+++ capítulo3/correcao da lista 1/exercicio2.py
@@ -1,3 +1,29 @@
-# main
-n = int(input("Digite um número de até 5 digitos:"))
+def removePrimeiroDigito(numero):
+    return numero // 10000
 
+def removeSegundoDigito(numero):
+    return (numero % 10000) // 1000
+
+def removeTerceiroDigito(numero):
+    return (numero % 1000) // 100
+
+def removeQuartoDigito(numero):
+    return (numero % 100) // 10
+
+def removeQuintoDigito(numero):
+    return numero % 10
+
+# main
+numero = int(input("Entre com um numero de 5 digitos:"))
+
+primeiro_digito = removePrimeiroDigito(numero)
+segundo_digito = removeSegundoDigito(numero)
+terceiro_digito = removeTerceiroDigito(numero)
+quarto_digito = removeQuartoDigito(numero)
+quinto_digito = removeQuintoDigito(numero)
+
+print(primeiro_digito)
+print(segundo_digito)
+print(terceiro_digito)
+print(quarto_digito)
+print(quinto_digito)
